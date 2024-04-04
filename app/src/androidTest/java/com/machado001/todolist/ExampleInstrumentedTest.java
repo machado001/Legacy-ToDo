3580a84f69755cd091be66867c272eb0556099f7
machado001 <machadowg@gmail.com>
2024-04-04 19:44:42 -0300

    test: add instrumented tests

--- app/src/androidTest/java/com/machado001/todolist/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/machado001/todolist/ExampleInstrumentedTest.java
@@ -1,14 +1,21 @@
 package com.machado001.todolist;
 
-import android.content.Context;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 
-import androidx.test.platform.app.InstrumentationRegistry;
+import android.os.Bundle;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.machado001.todolist.presentation.MainActivity;
+import com.machado001.todolist.presentation.MainFragment;
+
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,10 +24,25 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    private MainFragment frag;
+
+    @Rule
+    public final ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
+    @Before
+    public void setup(){
+        frag = MainFragment.newInstance("title one");
+        ActivityScenario.launchActivityForResult(MainActivity.class);
+    }
     @Test
-    public void useAppContext() {
+    public void newInstance_Should_create_different_instances() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.machado001.todolist", appContext.getPackageName());
+        MainFragment frag2 = MainFragment.newInstance("title two");
+        assertNotEquals(frag2,frag);
+    }
+
+    @Test
+    public void bundle_should_not_be_null_when_instantiating_fragment_directly() {
+        Bundle bundle = frag.getArguments();
+        assertNotNull(bundle);
     }
 }
